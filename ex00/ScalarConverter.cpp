ce954e8dfce3f7f14fd4334025a1e4ae7b7a6172
elsa2491 <elsa.touma@gmail.com>
2024-11-24 22:27:15 +0100

    handle long and float

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:16:13 by eltouma           #+#    #+#             */
-/*   Updated: 2024/11/24 21:20:36 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/11/24 22:26:52 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,15 @@ int	getInt(std::string s)
 
 	nb = strtol(s.c_str(), &endptr, 10);
 	if (*endptr != '\0')
-		return (false);
+		return (0);
 	if (nb < INT_MIN || nb > INT_MAX)
 		return (0);
-	return (nb);
+	return (1);
 }
 
 double	getDouble(std::string s)
 {
-	double	nb;
+	long double	nb;
 	char	*endptr;
 
 	nb = strtod(s.c_str(), &endptr);
@@ -54,12 +54,12 @@ double	getDouble(std::string s)
 		return (0);
 	if (nb < -DBL_MAX || nb > DBL_MAX)
 		return (0);
-	return (nb);
+	return (1);
 }
 
 float	getFloat(std::string s)
 {
-	double	nb;
+	long double	nb;
 	char	*endptr;
 
 	nb = strtof(s.c_str(), &endptr);
@@ -67,7 +67,7 @@ float	getFloat(std::string s)
 		return (0);
 	if (nb < -FLT_MAX || nb > FLT_MAX)
 		return (0);
-	return (nb);
+	return (1);
 }
 
 void	handleChar(std::string s)
@@ -97,6 +97,117 @@ int	getChar(std::string s)
 	return (0);
 }
 
+void	displayChar(double val)
+{
+	if (val < 32 || val > 126)
+		std::cout << "char: non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(val) << std::endl;
+}
+
+void	handleInt(std::string s)
+{
+	// char c;
+	long	nb;
+	float	f;
+	double	d;
+	char	*endptr;
+
+	nb = strtol(s.c_str(), &endptr, 10);
+	displayChar(nb);
+/*
+	if (nb < 32 || nb > 126)
+		std::cout << "char: non displayable" << std::endl;
+	else
+	{
+		c = static_cast<char>(nb);	
+		std::cout << "char: " << c << std::endl;
+	}
+*/
+	std::cout << "int: " << nb << std::endl;
+	f = static_cast<float>(nb);
+	std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f";
+	d = static_cast<double>(nb);
+	std::cout << "\ndouble: " << d << std::endl;
+	std::cout.unsetf(std::ios::fixed);
+}
+
+void	handleDouble(std::string s)
+{
+//	char	c;
+	long	nb;
+	float	flt;
+	double	dbl;
+	char	*endptr;
+
+	dbl = strtod(s.c_str(), &endptr);
+	displayChar(dbl);
+/*
+	if (dbl < 32 || dbl > 126)
+		std::cout << "char: non displayable" << std::endl;
+	else
+	{
+		c = static_cast<char>(dbl);	
+		std::cout << "char: " << c << std::endl;
+	}
+*/
+	if (dbl < INT_MIN || dbl > INT_MAX)
+		std::cout << "int: out of range" << std::endl;
+	else
+	{
+		nb = static_cast<int>(dbl);
+		std::cout << "int: " << nb << std::endl;
+	}
+	std::cout << std::fixed << std::showpoint;
+	if (dbl < -FLT_MAX || dbl > FLT_MAX)
+		std::cout << "float: out of range" << std::endl;
+	else
+	{
+		flt = static_cast<float>(dbl);
+		std::cout << "float: " << flt << "f" << std::endl;
+	}
+	std::cout << "double: " << dbl << std::endl;
+	std::cout.unsetf(std::ios::fixed | std::ios::showpoint);
+}
+
+void	handleFloat(std::string s)
+{
+//	char c;
+	long	nb;
+	float	flt;
+	double	dbl;
+	char	*endptr;
+	float	min;
+	float	max;
+
+	min = static_cast<float>(INT_MIN);
+	max = static_cast<float>(INT_MAX);
+	flt = strtof(s.c_str(), &endptr);
+	displayChar(flt);
+/*
+	if (flt < 32 || flt > 126)
+		std::cout << "char: non displayable" << std::endl;
+	else
+	{
+		c = static_cast<char>(flt);	
+		std::cout << "char: " << c << std::endl;
+	}
+*/
+	if (flt < min || flt > max)
+		std::cout << "int: out of range" << std::endl;
+	else
+	{
+		nb = static_cast<int>(flt);
+		std::cout << "int: " << nb << std::endl;
+	}
+	std::cout << std::fixed << std::showpoint;
+	flt = static_cast<float>(flt);
+	std::cout << "float: " << flt << "f" << std::endl;
+	dbl = static_cast<double>(flt);
+	std::cout << "double: " << dbl << std::endl;
+	std::cout.unsetf(std::ios::fixed | std::ios::showpoint);
+}
+
 void	ScalarConverter::convert(std::string s)
 {
 	std::cout << "convert " << s << std::endl;
@@ -107,17 +218,20 @@ void	ScalarConverter::convert(std::string s)
 		return ;
 	if (getInt(s))
 	{
-		std::cout << "c'est un int\n";
+		std::cout << "c'est un int\n\n";
+		handleInt(s);
 		return ;
 	}
 	if (getDouble(s))
 	{
-		std::cout << "c'est un double\n";
+		std::cout << "c'est un double\n\n";
+		handleDouble(s);
 		return ;
 	}
 	if (getFloat(s))
 	{
-		std::cout << "c'est un float\n";
+		std::cout << "c'est un float\n\n";
+		handleFloat(s);
 		return ;
 	}
 
